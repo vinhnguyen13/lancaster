9596155c988b518272bceb0167e51aadf4a7eaeb
vinh.nguyen <quangvinh.nguyen@trungthuygroup.vn>
2015-09-16 15:24:36 +0700

    Initial commit

--- common/config/db.php
+++ common/config/db.php
@@ -2,7 +2,7 @@
 
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost;dbname=metvuong',
+    'dsn' => 'mysql:host=localhost;dbname=lancaster',
     'username' => 'root',
     'password' => '',
     'charset' => 'utf8',
