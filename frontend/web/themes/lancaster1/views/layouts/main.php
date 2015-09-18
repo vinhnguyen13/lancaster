197ef1cde4a1affc709dac5dcec6cfca748e72cf
vinh.nguyen <quangvinh.nguyen@trungthuygroup.vn>
2015-09-18 14:13:37 +0700

    html statics

--- frontend/web/themes/lancaster1/views/layouts/main.php
+++ frontend/web/themes/lancaster1/views/layouts/main.php
@@ -82,10 +82,10 @@ AppAsset::register($this);
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
 <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
  -->
-<script src="<?=Yii::$app->view->theme->baseUrl?>/js/jquery.js"></script>
-<script src="<?=Yii::$app->view->theme->baseUrl?>/js/bootstrap.js"></script>
-<script src="<?=Yii::$app->view->theme->baseUrl?>/js/holder.htm"></script>
-<script src="<?=Yii::$app->view->theme->baseUrl?>/js/ie10-viewport-bug-workaround.js"></script>
+<script src="<?=Yii::$app->view->theme->baseUrl?>/resources/js/jquery.js"></script>
+<script src="<?=Yii::$app->view->theme->baseUrl?>/resources/js/bootstrap.js"></script>
+<script src="<?=Yii::$app->view->theme->baseUrl?>/resources/js/holder.htm"></script>
+<script src="<?=Yii::$app->view->theme->baseUrl?>/resources/js/ie10-viewport-bug-workaround.js"></script>
 
 <?php $this->endBody() ?>
 </body>
