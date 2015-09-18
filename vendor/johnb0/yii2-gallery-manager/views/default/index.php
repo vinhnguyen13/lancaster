c4772220c7aadf38d4c1f0e82c6d447ca81ce13a
vinh.nguyen <quangvinh.nguyen@trungthuygroup.vn>
2015-09-18 09:28:02 +0700

    gallery

--- vendor/johnb0/yii2-gallery-manager/views/default/index.php
+++ vendor/johnb0/yii2-gallery-manager/views/default/index.php
@@ -3,7 +3,7 @@
 ?>
 
 <div class="gallery-default-index">
-    <h2>Модуль Галерея</h2>
+    <h2>Gallery Module</h2>
     
     <div class="row-fluid text-center">
         <?= Html::a(Html::button('Index', ['class' => 'btn btn-default']), ['gallery/index']); ?>
