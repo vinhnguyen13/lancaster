52b9097615493156e39e521639d8a6c13f999ed6
Nhut Tran <nhut.tran@trungthuygroup.vn>
2015-09-21 17:58:27 +0700

    add code booking

--- vendor/vsoft/express/controllers/frontend/BookingController.php
+++ vendor/vsoft/express/controllers/frontend/BookingController.php
@@ -6,6 +6,7 @@
  */
 namespace vsoft\express\controllers\frontend;
 
+use vsoft\express\models\LcBuilding;
 use yii\web\Controller;
 
 class BookingController extends Controller
@@ -13,5 +14,30 @@ class BookingController extends Controller
     public function actionIndex()
     {
         return $this->render('index');
+    }
+
+    public function actionViewFloorByBuilding($id)
+    {
+        echo "<pre>";
+        print_r('Building id : '.$id);
+        echo "<pre>";
+        exit();
+        if($id > 0){
+            $building = LcBuilding::find()->where(['lc_building_id' => $id])->one();
+            $floor = $building->floor;
+            if($building){
+//                echo "<option value='333'>$building->floor</option>";
+                for($x = 1; $x <= $floor; $x++ ) {
+                    if($x === $floor)
+                        echo "<option selected value='$x'>$x</option>";
+                    else
+                        echo "<option value='$x'>$x</option>";
+                }
+
+            }
+        }
+//        else{
+//            echo "<option>Cannot find floor number in building</option>";
+//        }
     }
 }
