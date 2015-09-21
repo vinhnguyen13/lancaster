08b977b4f6f4c50ee7693ab0f9d7e90438ab6b93
Nhut Tran <nhut.tran@trungthuygroup.vn>
2015-09-21 18:01:40 +0700

    code DateTimePicker

---
+++ vendor/kartik-v/yii2-widget-datetimepicker-master/assets/js/locales/bootstrap-datetimepicker.vn.js
@@ -0,0 +1,18 @@
+/**
+ * Vietnam translation for bootstrap-datetimepicker
+ * NhutTran
+ */
+
+;(function($){
+    $.fn.datetimepicker.dates['vn'] = {
+        days:        ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"],
+        daysShort:   ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"],
+        daysMin:     ["T2", "T3", "Tu", "We", "Th", "Fr", "Sa", "Su"],
+        months:      ["January", "February", "March", "April", "May", "June", "July", "August", "Tháng 9", "October", "November", "December"],
+        monthsShort: ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Th9", "Oct", "Nov", "Dec"],
+        meridiem:    ["am", "pm"],
+        suffix:      ["st", "nd", "rd", "th"],
+        weekStart: 1,
+        today:       "Today"
+    };
+}(jQuery));
